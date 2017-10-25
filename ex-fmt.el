37241d442799a045b8c12a9acfa66abb8cff8f6a
Miguel Garcia <miguel.garcialafuente@cabify.com>
2017-10-25 19:57:54 +0200

    Use cfg file

--- ex-fmt.el
+++ ex-fmt.el
@@ -8,16 +8,39 @@
   (shell-command-to-string command))
 
 (defun ex-fmt--format-code (codepath)
-  (ex-fmt--execute (format "%s %s format %s" ex-fmt-elixir ex-fmt-mix codepath)))
+  (let* ((cfgpath (ex-fmt--find-file-in-hierarchy "." ".formatter.exs"))
+         (cmd (if cfgpath
+                  (format "%s %s format --dot-formatter %s %s" ex-fmt-elixir ex-fmt-mix cfgpath codepath)
+                (format "%s %s format %s" ex-fmt-elixir ex-fmt-mix codepath))))
+    (ex-fmt--execute cmd)))
 
 (defun ex-fmt--format-current-buffer ()
-  (ex-fmt--format-code buffer-file-name))
+  (interactive)
+  (when (and (not (eq nil ex-fmt-elixir)) (not (eq nil ex-fmt-mix)))
+    (ex-fmt--format-code buffer-file-name)
+    (revert-buffer :ignore-auto :noconfirm)))
+
+;; From http://stackoverflow.com/questions/14095189/walk-up-the-directory-tree
+(defun ex-fmt--parent-directory (dir)
+  "Go to the parent directory of `DIR' unless it's /."
+  (unless (equal "/" dir)
+    (file-name-directory (directory-file-name dir))))
+
+(defun ex-fmt--find-file-in-hierarchy (current-dir fname)
+  "Starting from `CURRENT-DIR', search for a file named `FNAME' upwards through the directory hierarchy."
+  (let ((file (concat current-dir fname))
+        (parent (jmr--parent-directory (expand-file-name current-dir))))
+    (if (file-exists-p file)
+        file
+      (when parent
+        (jmr--find-file-in-hierarchy parent fname)))))
 
 ;;;###autoload
-(defun ex-fmt-before-save ()
+(defun ex-fmt-after-save ()
   (interactive)
-  (when (eq major-mode 'elixir-mode) (ex-fmt--format-current-buffer)))
+  (when (eq major-mode 'elixir-mode)
+    (ex-fmt--format-current-buffer)))
 
-;; (add-hook 'before-save-hook #'ex-fmt-before-save)
+;; (add-hook 'after-save-hook #'ex-fmt-after-save)
 
 (provide 'ex-fmt)
